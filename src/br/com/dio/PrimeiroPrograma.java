673676ba64bf5941ef53aa8194462a479392e0fa
beatrizcardc <beacarcun@gmail.com>
2022-07-07 12:19:15 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,27 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+
+		System.out.println(gato);
+		System.out.println(livros);
+
+	}
+
+}
+
+/*
+ * abrindo classe fora do PrimeiroPrograma não precisa importar
+ */
+
+class Livros {
+	private String nome;
+	private String npag;
+
+}
